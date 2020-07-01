8de63575c8603f98ea8928e4d6b4f1f41e519bb1
Tony Mowers <tony@amowers.com>
2020-07-01 10:36:08 +0200

    change hello world text

--- SimpleCSharpProject/Program.cs
+++ SimpleCSharpProject/Program.cs
@@ -6,7 +6,7 @@ namespace SimpleCSharpProject
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
